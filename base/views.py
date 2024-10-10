8a427aac4e8685530f3175f44ffbe001cb09d1d2
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-10-10 23:48:42 +0500

    New Changes

--- base/views.py
+++ base/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms =[
-    {"id":1, "name": "One"},
-    {"id":2, "name": "Two"},
-    {"id":3, "name": "Three"},
-]
-
-def Home(request):
+def home(request):
+    rooms = Room.objects.all()
     context = {"rooms":rooms}
     return render(request,"base/home.html",context)
 
-def Room(request,pk):
-    selectedRoom = None
-    for room in rooms:
-        if room["id"] == int(pk):
-            selectedRoom = room
-    context = {"room": selectedRoom}
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
     return render(request,"base/room.html",context)
+
+def createRoom(request):
+    return render(request,"base/room_form.html",context={})
